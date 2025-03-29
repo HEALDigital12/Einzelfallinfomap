9d14cacaf8320b0f7a8d9c5a5ea83fed94f340e7
HEALDigital12 <healdigital12@gmail.com>
2025-03-29 15:37:36 +0100

    Update CrimeMap.jsx

--- src/CrimeMap.jsx
+++ src/CrimeMap.jsx
@@ -1,3 +1,3 @@
-﻿<xml version="1.0" encoding="utf-8"?><Error><Code>ServerBusy</Code><Message>Operations per second is over the account limit.
+﻿<xml version="1.0" encoding="utf-8"><Error><Code>ServerBusy</Code><Message>Operations per second is over the account limit.
 RequestId:c3419a6b-201e-0076-57b6-a040dd000000
 Time:2025-03-29T14:22:46.3172373Z</Message></Error>
