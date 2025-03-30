92b4336055cbeebf2935adae28cc50efb1d3a3e5
HEALDigital12 <healdigital12@gmail.com>
2025-03-30 11:58:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import CrimeMap from './CrimeMap';
 import Legend from './Legend';
 import Filter from './Filter';
+import YearFilter from './YearFilter';
+import LastUpdate from './LastUpdate';
 
 function App() {
   const [selectedTypes, setSelectedTypes] = useState([
@@ -10,12 +12,15 @@ function App() {
     'Raubüberfall',
     'Vergewaltigung'
   ]);
+  const [selectedYear, setSelectedYear] = useState(2025);
 
   return (
     <>
-      <CrimeMap selectedTypes={selectedTypes} />
+      <CrimeMap selectedTypes={selectedTypes} selectedYear={selectedYear} />
       <Legend />
       <Filter selectedTypes={selectedTypes} onChange={setSelectedTypes} />
+      <YearFilter selectedYear={selectedYear} onChange={setSelectedYear} />
+      <LastUpdate />
     </>
   );
 }
