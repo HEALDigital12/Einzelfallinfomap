abead76610ffb7b477c2246b96c34b888aa9e2d3
HEALDigital12 <healdigital12@gmail.com>
2025-05-08 16:13:14 +0200

    Update CrimeMap.jsx

--- src/CrimeMap.jsx
+++ src/CrimeMap.jsx
@@ -29,7 +29,7 @@ function CrimeMap({ selectedTypes, selectedYear }) {
     if (!mapRef.current) return;
 
     const fetchDataAndRender = () => {
-      fetch('/data/faelle_2025.json')
+      fetch('/public/data/faelle_2025.json')
         .then(res => res.json())
         .then(newData => {
           setData(newData);
