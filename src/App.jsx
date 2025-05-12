ebf7e16b5ab14b341cc43fb4954a6d005596ee56
HEALDigital12 <healdigital12@gmail.com>
2025-05-12 20:40:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import CrimeMap from './CrimeMap';
+import Legend from './Legend';
+import Filter from './Filter';
+import YearFilter from './YearFilter';
+import LastUpdate from './LastUpdate';
+
+function App() {
+  const [selectedTypes, setSelectedTypes] = useState([
+    'Tötung',
+    'Messerstecherei',
+    'Raub',
+    'Sexualdelikt'
+    // Hier sind die initial ausgewählten schweren Straftaten
+  ]);
+  const [selectedYear, setSelectedYear] = useState(2025);
+
+  // Optional: Initialisiere ausgewählte Typen nach dem Mounten
+  useEffect(() => {
+    setSelectedTypes(['Tötung', 'Messerstecherei', 'Raub', 'Sexualdelikt']);
+  }, []);
+
+  return (
+    <>
+      <CrimeMap selectedTypes={selectedTypes} selectedYear={selectedYear} />
+      <Legend />
+      <Filter selectedTypes={selectedTypes} onChange={setSelectedTypes} />
+      <YearFilter selectedYear={selectedYear} onChange={setSelectedYear} />
+      <LastUpdate />
+    </>
+  );
+}
+
+export default App;
 import React, { useState } from 'react';
 import CrimeMap from './CrimeMap';
 import Legend from './Legend';
