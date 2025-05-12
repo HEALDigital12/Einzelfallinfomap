2bba2ce3782f52380a72fbfc59a5f667e978d3e9
HEALDigital12 <healdigital12@gmail.com>
2025-05-12 20:44:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,30 +32,3 @@ function App() {
 }
 
 export default App;
-import React, { useState } from 'react';
-import CrimeMap from './CrimeMap';
-import Legend from './Legend';
-import Filter from './Filter';
-import YearFilter from './YearFilter';
-import LastUpdate from './LastUpdate';
-
-function App() {
-  const [selectedTypes, setSelectedTypes] = useState([
-    'Verkehrsunfall',
-    'Einbruch'
-    // Füge hier weitere Delikttypen hinzu, die in deiner JSON vorkommen.
-  ]);
-  const [selectedYear, setSelectedYear] = useState(2025);
-
-  return (
-    <>
-      <CrimeMap selectedTypes={selectedTypes} selectedYear={selectedYear} />
-      <Legend />
-      <Filter selectedTypes={selectedTypes} onChange={setSelectedTypes} />
-      <YearFilter selectedYear={selectedYear} onChange={setSelectedYear} />
-      <LastUpdate />
-    </>
-  );
-}
-
-export default App;
